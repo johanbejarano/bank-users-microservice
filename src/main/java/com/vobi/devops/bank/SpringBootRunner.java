ddb9257f72d80cd7e0e802dd35b39a3f46d9b0d2
Johan Bejarano <jbejarano@vortexbird.com>
2021-04-15 14:33:22 -0500

    Los microservicios se registran y se consumen por Eureka

--- src/main/java/com/vobi/devops/bank/SpringBootRunner.java
+++ src/main/java/com/vobi/devops/bank/SpringBootRunner.java
@@ -2,6 +2,7 @@ package com.vobi.devops.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Zathura Code Generator Version 9.0 http://zathuracode.org/
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRunner.class, args);
